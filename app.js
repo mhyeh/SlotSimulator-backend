c3a3dbf89253a15e2ccd813b79b99c01c0be2e2d
blackkite0206233 <samuel991640@gmail.com>
2017-08-01 18:33:04 +0800

    add router

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var Chart = require('./routes/Chart');
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', Chart);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/Chart.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/rtp', function(req, res, next) {
+  
+});
+
+router.get('/totalNetWin', function(req, res, next) {
+
+});
+
+router.get('/survivalRate', function(req, res, next) {
+  
+});
+
+
+module.exports = router;
