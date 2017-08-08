8f078143fff3f07b0b605e04f14c9c904dc3634b
blackkite0206233 <samuel991640@gmail.com>
2017-08-08 18:03:33 +0800

    sql get rtp

--- ChartGenerator/Repositories/DataRepository.js
+++ ChartGenerator/Repositories/DataRepository.js
@@ -1,12 +1,28 @@
 let Promise = require('bluebird')
 
-let getRTP = function (projectId, request) {
-  let size  = request.size
-  let step  = request.step
-  let range = request.range
+let model = require('../connect')
 
+let getRTP = function (projectId, request) {
   return new Promise((resolve, reject) => {
-    let result = {}
+    let size  = request.size
+    let step  = request.step
+    let range = request.range
+
+    let result = []
+    model.knex('overall').select(model.knex.row('((sum(`netWin`) + ?) / ?) as rtp, floor( (`id` - 1) / ?) as `group`', 
+      [size * step, step, step])).where('id', '<', size).groupBy('group').orderBy('rtp', 'asc')
+    .then(rtpSet => {
+      for (let rtp of rtpSet) {
+        while (rtp.rtp / range >= result.length) {
+          result.push(0)
+        }
+        result[rtp.rtp / range]++
+      }
+      resolve(result)
+    }).catch(error => {
+      console.log(error)
+      reject()
+    })
   })
 }
 
