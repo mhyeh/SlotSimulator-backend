2eb19b592f7b864f89ffbb1a80be691ea0fbd0aa
blackkite0206233 <samuel991640@gmail.com>
2017-08-09 15:48:53 +0800

    allow cors header

--- app.js
+++ app.js
@@ -15,8 +15,14 @@ let errorMsgService = require('./ChartGenerator/Services/ErrorMsgService')
 
 let app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
+let allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
@@ -25,6 +31,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(allowCrossDomain())
 
 app.use('/account', Account)
 
