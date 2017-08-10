9f73e20aa5224723c44d482b7b456bef288d2a82
blackkite0206233 <samuel991640@gmail.com>
2017-08-10 15:31:41 +0800

    Update CORS

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
   res.set({'Access-Control-Allow-Origin':  '*',
-           'Access-Control-Allow-Headers': 'Content-Type' 
+           'Access-Control-Allow-Headers': 'Content-Type,Authorization' 
   })
 
   next()
