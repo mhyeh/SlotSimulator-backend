ad0d745ac91a64efdf6550c52906216465e72c99
BlackKite0206233 <samuel991640@gmail.com>
2017-08-12 22:55:16 +0800

    add Table API

---
+++ ChartGenerator/Services/TableService.js
@@ -0,0 +1,81 @@
+let Promise = require('bluebird')
+
+let redisRepository = require('../Repositories/RedisRepository')
+
+let fileService     = require('./FileService')
+let errorMsgService = require('./ErrorMsgService')
+
+let extension = '.csv'
+let folder    = './userProject/'
+
+let getOverAll = function (token, id) {
+  return new Promise((resolve, reject) => {
+    redisRepository.getAccountId(token).then(accountId => {
+      let path = folder + accountId + '/' + id  + '/'
+      let promise = {}
+      promise.simulation = fileService.readFile(path + 'overAll'       + extension)
+      promise.theory     = fileService.readFile(path + 'overAllTheory' + extension)
+      return Promise.props(promise)
+    }).then(result => {
+      resolve(result)
+    }).catch(error => {
+      if (error === 'token expired') {
+        reject(errorMsgService.tokenExpired)
+      } else if (error === 'file error') {
+        reject(errorMsgService.fsError)
+      } else {
+        reject(errorMsgService.serverError)
+      }
+    })
+  })
+}
+
+let getBaseGame = function (token, id) {
+  return new Promise((resolve, reject) => {
+    redisRepository.getAccountId(token).then(accountId => {
+      let path = folder + accountId + '/' + id  + '/'
+      let promise = {}
+      promise.simulation = fileService.readFile(path + 'baseGame'       + extension)
+      promise.theory     = fileService.readFile(path + 'baseGameTheory' + extension)
+      return Promise.props(promise)
+    }).then(result => {
+      resolve(result)
+    }).catch(error => {
+      if (error === 'token expired') {
+        reject(errorMsgService.tokenExpired)
+      } else if (error === 'file error') {
+        reject(errorMsgService.fsError)
+      } else {
+        reject(errorMsgService.serverError)
+      }
+    })
+  })
+}
+
+let getFreeGame = function (token, id) {
+  return new Promise((resolve, reject) => {
+    redisRepository.getAccountId(token).then(accountId => {
+      let path = folder + accountId + '/' + id  + '/'
+      let promise = {}
+      promise.simulation = fileService.readFile(path + 'freeGame'       + extension)
+      promise.theory     = fileService.readFile(path + 'freeGameTheory' + extension)
+      return Promise.props(promise)
+    }).then(result => {
+      resolve(result)
+    }).catch(error => {
+      if (error === 'token expired') {
+        reject(errorMsgService.tokenExpired)
+      } else if (error === 'file error') {
+        reject(errorMsgService.fsError)
+      } else {
+        reject(errorMsgService.serverError)
+      }
+    })
+  })
+}
+
+module.exports = {
+  getOverAll:  getOverAll,
+  getBaseGame: getBaseGame,
+  getFreeGame: getFreeGame
+}
---
+++ routes/Table.js
@@ -0,0 +1,29 @@
+let express = require('express')
+
+let tableService = require('../ChartGenerator/Services/TableService')
+
+let router = express.Router()
+
+router.get('/:id/overAll', (req, res, next) => {
+    tableService.getOverAll(req.get('Authorization'), req.params.id).then(result => {
+    res.status(200).json(result)
+  }).catch(error => {
+    res.status(400).json(error)
+  })
+})
+
+router.get('/:id/baseGame', (req, res, next) => {
+    tableService.getBaseGame(req.get('Authorization'), req.params.id).then(result => {
+    res.status(200).json(result)
+  }).catch(error => {
+    res.status(400).json(error)
+  })
+})
+
+router.get('/:id/FreeGame', (req, res, next) => {
+    tableService.getFreeGame(req.get('Authorization'), req.params.id).then(result => {
+    res.status(200).json(result)
+  }).catch(error => {
+    res.status(400).json(error)
+  })
+})
