8b8414eaa8dc6e0f6af3be1b437b6b1dae42e154
blackkite0206233 <samuel991640@gmail.com>
2017-09-18 17:10:53 +0800

    fix some bugs

--- ChartGenerator/Services/UploadService.js
+++ ChartGenerator/Services/UploadService.js
@@ -1,15 +1,15 @@
 let Promise    = require('bluebird')
 let formidable = require('formidable')
 let path       = require('path')
-let fs         = Promise.promisifyAll(require('fs'))
+let fs         = require('fs')
 
 let redisRepository = require('../Repositories/RedisRepository')
 
 let fileService     = require('./FileService')
 let errorMsgService = require('./ErrorMsgService')
 
-let appRoot = path.join(path.dirname(require.main.filename), '../')
-
+let appRoot   = path.join(path.dirname(require.main.filename), '../userProject/')
+let extension = '.csv'
 // let filesName = ['baseSimPar', 'bonusSimPar', 'overallSimPar', 'baseTheoryPar', 'bonusTheoryPar', 'overallTheoryPar', 'baseSpinData', 'bonusSpinData', 'overallSpinData', 'overallSurvivalRate']
 
 let uploadFile = function(token, id, data) {
@@ -17,20 +17,22 @@ let uploadFile = function(token, id, data) {
     redisRepository.getAccountId(token).then(accountId => {
       let dir = appRoot + accountId + '/' + id + '/'
       let form = new formidable.IncomingForm()
-      form.encoding  = 'utf-8'
-      form.uploadDir = dir
-      form.type      = true
-      form.multiples = false
-  
-      form.parse(req, (err, fields, files) => {
+      form.encoding       = 'utf-8'
+      form.uploadDir      = dir
+      form.keepExtensions = true
+      form.multiples      = false
+      form.parse(data, (err, fields, files) => {
         if (err) {
+          console.log('error')
           reject(errorMsgService.fsError)
           return
         }
-        fs.rename(dir + files[fileName].name + extension, dir + data.body.name + extension).then(() => {
+        fs.rename(files[fields.name].path, dir + fields.name + extension, (err) => {
+          if (err) {
+            reject(errorMsgService.fsError)
+            return
+          }
           resolve()
-        }).catch(error => {
-          reject(errorMsgService.fsError)
         })
       })
     }).catch(error => {
--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -5,7 +5,7 @@ let uploadService = require('../ChartGenerator/Services/UploadService')
 let router = express.Router()
 
 router.put('/:id', (req, res, next) => {
-  uploadService.uploadFile(req.get('Authorization'), req.params.id, res).then(() => {
+  uploadService.uploadFile(req.get('Authorization'), req.params.id, req).then(() => {
     res.status(200).json({message: 'success'})
   }).catch(error => {
     res.status(400).json(error)
