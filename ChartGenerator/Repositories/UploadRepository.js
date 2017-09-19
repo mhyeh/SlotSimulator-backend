141fa99e1d9a050caf99fd00ca31875f6ba39f3c
blackkite0206233 <samuel991640@gmail.com>
2017-09-19 16:51:12 +0800

    modify SQL qiery

--- ChartGenerator/Repositories/UploadRepository.js
+++ ChartGenerator/Repositories/UploadRepository.js
@@ -5,7 +5,7 @@ let model = require('../connect')
 let upload = function (id, table, path, field) {
   return new Promise((resolve, reject) => {
     model.knex(table + id).del().then(() => {
-      let query = 'alter table ' + table + id + ' AUTO_INCREMENT = 1'
+      let query = 'truncate ' + table + id
       return model.knex.raw(query)
     }).then(() => {
       let query = 'load data local infile \'' + path + '\' into table ' + table + id + ' (' + field + ')'
