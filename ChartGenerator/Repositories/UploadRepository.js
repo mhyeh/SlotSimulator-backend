5e97acc22e331d4ff8520e69935240ae762d48bc
blackkite0206233 <samuel991640@gmail.com>
2017-09-19 15:59:52 +0800

    fix SQL query

--- ChartGenerator/Repositories/UploadRepository.js
+++ ChartGenerator/Repositories/UploadRepository.js
@@ -5,7 +5,8 @@ let model = require('../connect')
 let upload = function (id, table, path, field) {
   return new Promise((resolve, reject) => {
     model.knex(table + id).del().then(() => {
-      return model.knex.raw('load data local infile ? into table ' + table + id + ' (' + field + ')', path)
+      let query = 'load data local infile \'' + path + '\' into table ' + table + id + ' (' + field + ')'
+      return model.knex.raw(query)
     }).then(() => {
       resolve()
     }).catch(error => {
