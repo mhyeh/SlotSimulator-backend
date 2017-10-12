b0f8bf1aea0e4520b501e4b2071e95a5d8fe67da
blackkite0206233 <samuel991640@gmail.com>
2017-10-12 13:26:27 +0800

    fix some bugs

--- routes/Chart.js
+++ routes/Chart.js
@@ -34,7 +34,7 @@ router.get('/:id/freeGame', (req, res, next) => {
 */
 
 router.get('/:id/distribution', (req, res, next) => {
-  chartService.getFreeGame(req.get('Authorization'), req.params.id, req.query).then(result => {
+  chartService.getDistribution(req.get('Authorization'), req.params.id, req.query).then(result => {
     res.status(200).json(result)
   }).catch(error => {
     res.status(400).json(error)
