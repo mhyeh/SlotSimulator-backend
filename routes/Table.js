44c7b3861a89289a31f0048c2a3d7895ecc508b3
blackkite0206233 <samuel991640@gmail.com>
2017-10-12 12:56:06 +0800

    fix some bugs

--- routes/Table.js
+++ routes/Table.js
@@ -62,7 +62,7 @@ router.get('/:id/FreeGameTheory', (req, res, next) => {
 
 router.get('/:id/:type', (req, res, next) => {
   tableService.getTable(req.get('Authorization'), req.params.id, req.params.type).then(result => {
-    res.status(200).json({theory: result})
+    res.status(200).json({table: result})
   }).catch(error => {
     res.status(400).json(error)
   })
