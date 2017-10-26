112b4e600c2e3306b85199ea99b56a6163e2e7e2
blackkite0206233 <samuel991640@gmail.com>
2017-10-26 13:07:59 +0800

    fix some bugs

--- routes/AnalysisData.js
+++ routes/AnalysisData.js
@@ -4,7 +4,7 @@ let dataService = require('../ChartGenerator/Services/DataService')
 
 let router = express.Router()
 
-router.get('chart/:id/distribution', (req, res, next) => {
+router.get('/chart/:id/distribution', (req, res, next) => {
   dataService.getDistribution(req.get('Authorization'), req.params.id, req.query).then(result => {
     res.status(200).json(result)
   }).catch(error => {
@@ -13,7 +13,7 @@ router.get('chart/:id/distribution', (req, res, next) => {
 })
 
 // get player experience (每 400 筆紀錄一次 RTP)
-router.get('chart/:id/rtp', (req, res, next) => {
+router.get('/chart/:id/rtp', (req, res, next) => {
   dataService.getRTP(req.get('Authorization'), req.params.id, req.query).then(result => {
     res.status(200).json(result)
   }).catch(error => {
@@ -22,7 +22,7 @@ router.get('chart/:id/rtp', (req, res, next) => {
 })
 
 // get player experience (觸發 free game 壓力)
-router.get('chart/:id/totalNetWin', (req, res, next) => {
+router.get('/chart/:id/totalNetWin', (req, res, next) => {
   dataService.getTotalNetWin(req.get('Authorization'), req.params.id, req.query).then(result => {
     res.status(200).json(result)
   }).catch(error => {
@@ -31,7 +31,7 @@ router.get('chart/:id/totalNetWin', (req, res, next) => {
 })
 
 // get player experience (存活率分析)
-router.get('chart/:id/survivalRate', (req, res, next) => {
+router.get('/chart/:id/survivalRate', (req, res, next) => {
   dataService.getSurvivalRate(req.get('Authorization'), req.params.id, req.query).then(result => {
     res.status(200).json(result)
   }).catch(error => {
@@ -39,7 +39,7 @@ router.get('chart/:id/survivalRate', (req, res, next) => {
   })
 })
 
-router.get('table/:id/:type', (req, res, next) => {
+router.get('/table/:id/:type', (req, res, next) => {
   dataService.getTable(req.get('Authorization'), req.params.id, req.params.type).then(result => {
     res.status(200).json({table: result})
   }).catch(error => {
