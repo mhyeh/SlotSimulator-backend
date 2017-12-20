d9c0160d3079ff6fa1d9c47b9de1488d66502ce5
blackkite0206233 <samuel991640@gmail.com>
2017-12-20 16:01:04 +0800

    test tr

--- ChartGenerator/Services/FileService.js
+++ ChartGenerator/Services/FileService.js
@@ -88,18 +88,23 @@ let processFormData = function (data) {
         reject('file error')
         return
       }
-      console.log(files)
-      // let promise = []
-      // for (let file of files) {
-      //   promise.push(child_process.exec('tr -d \\r < ' + file.path + ' > ' + file.path))
-      // }
-      // Promise.all(promise).then(() => {
-      //   resolve({fields: fields, files: files})
-      // }).catch(error => {
-      //   console.log(error)
-      //   reject()
-      // })
-      resolve({fields: fields, files: files})
+
+      let promise = []
+      for (let i in files) {
+        if (Array.isArray(fils[i])) {
+          for (let j in files[i]) {
+            promise.push(child_process.exec('tr -d \\r < ' + files[i][j].path + ' > ' + files[i][j].path))
+          }
+        } else {
+          promise.push(child_process.exec('tr -d \\r < ' + files[i].path + ' > ' + files[i].path))
+        }
+      }
+      Promise.all(promise).then(() => {
+        resolve({fields: fields, files: files})
+      }).catch(error => {
+        console.log(error)
+        reject()
+      })
     })
   })
 }
